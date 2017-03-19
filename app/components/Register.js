5dae3a1e4e217d668d15ede9c531e6d4efb20dba
rjycpsndl <dalaarjay@gmail.com>
2017-03-19 21:56:42 +0800

    fix username auth

--- app/components/Register.js
+++ app/components/Register.js
@@ -31,7 +31,7 @@ class Register extends Component {
     }
   }
   onUsername(e){
-    var regex= /^(?=.*\d.*)[A-Za-z0-9]{4,20}$/;
+    var regex= /^(?=.*\d)(?=.*[A-Za-z])[A-Za-z0-9]{5,20}$/;
     // var regex= /([A-Za-z0-9]){4,20}$/;
     if(regex.test(this.refs.username.value,) ===false){
         this.setState({
--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -47832,7 +47832,7 @@ var Register = function (_Component) {
   }, {
     key: 'onUsername',
     value: function onUsername(e) {
-      var regex = /^(?=.*\d.*)[A-Za-z0-9]{4,20}$/;
+      var regex = /^(?=.*\d)(?=.*[A-Za-z])[A-Za-z0-9]{5,20}$/;
       // var regex= /([A-Za-z0-9]){4,20}$/;
       if (regex.test(this.refs.username.value) === false) {
         this.setState({
